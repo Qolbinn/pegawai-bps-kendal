5bd53493fb6291b1ec37bc5f864024690e8df77c
unknown <222111926@stis.ac.id>
2024-07-30 13:31:30 +0700

    Inisiasi Projek

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { type Application, type NextFunction, type Request, type Response } from 'express'
+import 'dotenv/config'
+
+const app: Application = express()
+const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
+})
+
